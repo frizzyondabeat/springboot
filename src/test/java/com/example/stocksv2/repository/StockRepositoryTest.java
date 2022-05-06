0f145fefa46bfc63643b7ae21d768053affb3521
frizzyondabeat <frizzyondabeat@gmail.com>
2022-05-06 18:59:26 +0100

    Completion of Repository Unit test

--- src/test/java/com/example/stocksv2/repository/StockRepositoryTest.java
+++ src/test/java/com/example/stocksv2/repository/StockRepositoryTest.java
@@ -1,62 +1,90 @@
 package com.example.stocksv2.repository;
 
 import com.example.stocksv2.model.Stock;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class StockRepositoryTest {
-
     @Autowired
     private StockRepository underTest;
 
+    @Nested
+    class whenFindingByName {
+        @Test
+        void itShouldFindByName() {
+            String NAME = "XRP";
+            assertThat(underTest.findByName(NAME).get(0)).isExactlyInstanceOf(Stock.class);
+        }
 
-    @Test
-    void itShouldFindByName() {
-        String NAME = "XRP";
-        assertThat(underTest.findByName(NAME)).isExactlyInstanceOf(List.of(Stock.class));
+        @BeforeEach
+        void setup() {
+            Stock stock = new Stock(
+                    null,
+                    "XRP",
+                    14532.89,
+                    Timestamp.valueOf(LocalDateTime.now())
+            );
+            underTest.save(stock);
+        }
     }
 
-    @BeforeEach
-    void setup() {
-        Stock stock = new Stock(
-                null,
-                "XRP",
-                14532.89,
-                Timestamp.valueOf(LocalDateTime.now())
-        );
-        underTest.save(stock);
+
+
+    @Nested
+    class WhenExistingByName {
+        @Test
+        void itShouldExistByName(){
+            String NAME = "GRU";
+            assertThat(underTest.existsByName(NAME)).isTrue();
+        }
+
+        @Test
+        void itShouldNotExistByName(){
+            String NAME = "JAY";
+            assertThat(underTest.existsByName(NAME)).isFalse();
+        }
+
+        @BeforeEach
+        void setup() {
+            Stock stock = new Stock(
+                    null,
+                    "GRU",
+                    149532.59,
+                    Timestamp.valueOf(LocalDateTime.now())
+            );
+            underTest.save(stock);
+        }
     }
 
+    @Nested
+    class WhenFindingByAmount {
 
-//    @Nested
-//    class WhenExistsingByName {
-//        private final String NAME = "NAME";
-//
-//        @BeforeEach
-//        void setup() {
-//        }
-//    }
-//
-//    @Nested
-//    class WhenFindingByAmount {
-//        @BeforeEach
-//        void setup() {
-//        }
-//    }
+        @Test
+        void itShouldFindByAmount(){
+            Double amount = 10006532.99;
+            assertThat(underTest.findByAmount(amount).get(0)).isExactlyInstanceOf(Stock.class);
+        }
+
+        @BeforeEach
+        void setup() {
+            Stock stock = new Stock(
+                    null,
+                    "FRC",
+                    10006532.99,
+                    Timestamp.valueOf(LocalDateTime.now())
+            );
+            underTest.save(stock);
+        }
+    }
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
